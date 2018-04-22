fd60053398a96c55749ccdf6107605f6b1001f49
Dr. Steffen Wendzel <wendzel@cs.uni-bonn.de>
2018-04-22 16:19:19 +0200

    remove 2.x version stuff

--- getver.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# extract version and release name and print to stdout
-echo -n `egrep ' VERSION| RELEASENAME' src/include/main.h | \
-	grep -v WELCOMEVER | awk '{print $3}'` | tr [\"\'] ' ' | \
-	awk '{print $1 " " $2}'
-
-
