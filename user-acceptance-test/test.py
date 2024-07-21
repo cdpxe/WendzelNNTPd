18faef56ec422d92b332d3e647774336dc9da936
Sven Liebert <mail@sven-liebert.de>
2024-07-21 18:57:55 +0200

    Update test and add recording

--- user-acceptance-test/test.py
+++ user-acceptance-test/test.py
@@ -1,27 +1,42 @@
-# Python3 + PyTest
+import unittest
 import pytest
-
+import base64
+from appium.options.android import UiAutomator2Options
 from appium import webdriver
 # Options are available in Python client since v2.6.0
 from appium.options.windows import WindowsOptions
 
-def generate_options():
-    classic_options = WindowsOptions()
-    classic_options.app = 'C:\\Program Files\\Mozilla Thunderbird\\thunderbird.exe'
-    # Make sure arguments are quoted/escaped properly if necessary:
-    # https://ss64.com/nt/syntax-esc.html
+appium_server_url = 'http://127.0.0.1:4723'
 
-    return [classic_options]
+capabilities = dict(
+    platformName='Windows',
+    automationName='Windows',
+    app='C:\\Program Files\\Mozilla Thunderbird\\thunderbird.exe'
+)
 
+class TestAppium(unittest.TestCase):
 
-@pytest.fixture(params=generate_options())
-def driver(request):
-    # The default URL is http://127.0.0.1:4723/wd/hub in Appium 1
-    drv = webdriver.Remote('http://127.0.0.1:4723', options=request.param)
-    yield drv
-    drv.quit()
+    def setUp(self) -> None:
+        self.driver = webdriver.Remote(appium_server_url, options=UiAutomator2Options().load_capabilities(capabilities))
+        self.driver.start_recording_screen()
 
-def test_app_source_could_be_retrieved(driver):
-    element = driver.find_element('name', 'AppMenu')
-    element.click()
-    assert driver.find_element('name', 'Account Settings')
+    def save_video(self, video, filename):
+        log_folder = 'D:\\a\\SvenLie\\WendzelNNTPd\\user-acceptance-test\\videos'
+        video_path = f'{log_folder}\\{filename}.mp4'
+        with open(video_path, 'wb') as output_file:
+            output_file.write(base64.b64decode(video))
+
+    def tearDown(self) -> None:
+        self.save_video(self.driver.stop_recording_screen(), 'test')
+        if self.driver:
+            self.driver.quit()
+
+    def test_find_battery(self) -> None:
+        self.driver.find_element('name', 'AppMenu').click()
+        assert self.driver.find_element('name', 'New Account')
+        self.driver.find_element('name', 'New Account').click()
+        assert self.driver.find_element('name', 'Newsgroup')
+        self.driver.find_element('name', 'Newsgroup').click()
+
+if __name__ == '__main__':
+    unittest.main()
