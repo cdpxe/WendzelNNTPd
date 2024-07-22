f0a0ba14eb6001713edfc6579126ad208306d88f
Sven Liebert <mail@sven-liebert.de>
2024-07-22 13:31:31 +0200

    Try to install docker

--- user-acceptance-test/test.py
+++ user-acceptance-test/test.py
@@ -5,6 +5,8 @@ from appium.options.android import UiAutomator2Options
 from appium import webdriver
 # Options are available in Python client since v2.6.0
 from appium.options.windows import WindowsOptions
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
 
 appium_server_url = 'http://127.0.0.1:4723'
 
@@ -18,11 +20,12 @@ class TestAppium(unittest.TestCase):
 
     def setUp(self) -> None:
         self.driver = webdriver.Remote(appium_server_url, options=UiAutomator2Options().load_capabilities(capabilities))
+        self.driver.implicitly_wait(10)
         self.driver.start_recording_screen()
 
     def save_video(self, video, filename):
         log_folder = 'D:\\a\\WendzelNNTPd\\WendzelNNTPd\\user-acceptance-test\\videos'
-        video_path = f'{log_folder}\\{filename}.mp4'
+        video_path = f'{log_folder}/{filename}.mp4'
         with open(video_path, 'wb') as output_file:
             output_file.write(base64.b64decode(video))
 
@@ -32,11 +35,19 @@ class TestAppium(unittest.TestCase):
             self.driver.quit()
 
     def test_find_battery(self) -> None:
-        self.driver.find_element('name', 'AppMenu').click()
-        assert self.driver.find_element('name', 'New Account')
-        self.driver.find_element('name', 'New Account').click()
-        assert self.driver.find_element('name', 'Newsgroup')
+        self.driver.find_element('name', 'Anwendungsmenü').click()
+        self.driver.find_element('name', 'Neues Konto').click()
         self.driver.find_element('name', 'Newsgroup').click()
+        self.driver.find_element(By.XPATH, '//Edit[@Name="Ihr Name:"]').send_keys('Testautomatisierung')
+        self.driver.find_element(By.XPATH, '//Edit[@Name="E-Mail-Adresse:"]').send_keys('testautomatisierung@test.de')
+        self.driver.find_element(By.XPATH, '//Window[@AutomationId="AccountWizard"]/Button[contains(@Name, "Weiter")]').click()
+
+        self.driver.find_element(By.XPATH, '//Edit[@Name="Newsgruppen-Server:"]').send_keys('127.0.0.1')
+        self.driver.find_element(By.XPATH, '//Window[@AutomationId="AccountWizard"]/Button[contains(@Name, "Weiter")]').click()
+        
+        self.driver.find_element(By.XPATH, '//Window[@AutomationId="AccountWizard"]/Button[contains(@Name, "Weiter")]').click()
+
+        self.driver.find_element(By.XPATH, '//Window[@AutomationId="AccountWizard"]/Button[contains(@Name, "Fertigstellen")]').click()
 
 if __name__ == '__main__':
     unittest.main()
