478d213875d03d23f7ac45a44dc3c01636200740
Ole Ludwig <mail@oleludwig.de>
2025-06-24 17:22:50 +0200

    Improve output of run.sh

--- unittesting/tls-testing/run.sh
+++ unittesting/tls-testing/run.sh
@@ -1,7 +1,20 @@
 error=0
+results=()
+
 for file in *.exp
 do
-    expect $file || error=1
+    echo "Run test $file"
+    expect $file
+    result=$?
+    if [ $result -ne 0 ]
+    then
+        error=1
+    fi
+    results+=("$file: $result")
+    echo ""
 done
+
+echo "Test results in the format <filename>: <returncode>"
+printf '%s\n' "${results[@]}"
 
 exit $error
