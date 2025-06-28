4de0a75c40fdb60b64820d1a8e7e1e2c01c01789
Ole Ludwig <mail@oleludwig.de>
2025-06-28 13:34:00 +0200

    Add script for the initialization of the database for the tests
    Run the script in the github workflow
    Adapt tests for the new testdata

---
+++ unittesting/tls-testing/create_db_test_data.sql
@@ -0,0 +1,33 @@
+INSERT INTO newsgroups (id, name, pflag, high) VALUES (1, 'alt.wendzelnntpd.test', 'y', 2);
+INSERT INTO newsgroups (id, name, pflag, high) VALUES (2, 'alt.wendzelnntpd.test.empty', 'y', 0);
+INSERT INTO newsgroups (id, name, pflag, high) VALUES (3, 'alt.wendzelnntpd.test.post', 'y', 0);
+
+INSERT INTO postings (msgid, date, author, newsgroups, subject, lines, header)
+VALUES ('<cdp1@localhost>', 1751106600, 'Testuser <test@example.com>', 'alt.wendzelnntpd.test',
+        'Test', 1, 'Path: test
+Message-ID: <cdp1@localhost>
+Date: Sat, 28 Jun 2025 12:30:00 +0200
+Lines: 1
+X-WendzelNNTPdBodysize: 9
+newsgroups: alt.wendzelnntpd.test
+from: Testuser <test@example.com>
+subject: Test
+');
+
+INSERT INTO postings (msgid, date, author, newsgroups, subject, lines, header)
+VALUES ('<cdp2@localhost>', 1751106601, 'Testuser <test@example.com>', 'alt.wendzelnntpd.test',
+        'Test', 1, 'Path: test
+Message-ID: <cdp2@localhost>
+Date: Sat, 28 Jun 2025 12:30:01 +0200
+Lines: 1
+X-WendzelNNTPdBodysize: 10
+newsgroups: alt.wendzelnntpd.test
+from: Testuser <test@example.com>
+subject: Test2
+');
+
+INSERT INTO ngposts (msgid, ng, postnum) VALUES ('<cdp1@localhost>', 'alt.wendzelnntpd.test', 1);
+INSERT INTO ngposts (msgid, ng, postnum) VALUES ('<cdp2@localhost>', 'alt.wendzelnntpd.test', 2);
+
+-- create user with name "testuser" and password "password"
+INSERT INTO users (name, password) VALUES ('testuser', 'ff36ffffffffffffff0fff3cff32ffff794d4815ffff02ffff6fffff40ffff7d');
---
+++ unittesting/tls-testing/initialize_db.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+
+DATA_DIR=/var/spool/news/wendzelnntpd
+DB=$DATA_DIR/usenet.db
+
+# Cleanup data directory
+rm $DATA_DIR/*
+
+# Initialize database structure
+cat ../../database/usenet.db_struct | sqlite3 $DB
+
+# Create test data in the database:
+# 3 newgroups: "alt.wendzelnntpd.test", "alt.wendzelnntpd.test.empty" and "alt.wendzelnntpd.test.post"
+# 2 postings in newsgroup "alt.wendzelnntpd.test"
+# 1 user with the name "testuser" and password "password"
+cat create_db_test_data.sql | sqlite3 $DB
+
+# Copy existing posting
+cp test-files/cdp?@localhost $DATA_DIR/
+
+# Initialize nextmsgid with 2, because there are already 2 postings in the database
+printf '\x02\x00\x00\x00\x00\x00\x00\x00' > $DATA_DIR/nextmsgid
