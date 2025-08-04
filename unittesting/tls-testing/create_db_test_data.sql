888ce0663c51aea3eb236a46ef3af389f8909e37
Ole Ludwig <mail@oleludwig.de>
2025-08-04 02:02:47 +0200

    Add tests for acl
    Add testdata for acl to the testdatabase including a second user.

--- unittesting/tls-testing/create_db_test_data.sql
+++ unittesting/tls-testing/create_db_test_data.sql
@@ -31,3 +31,10 @@ INSERT INTO ngposts (msgid, ng, postnum) VALUES ('<cdp2@localhost>', 'alt.wendze
 
 -- create user with name "testuser" and password "password"
 INSERT INTO users (name, password) VALUES ('testuser', 'ff36ffffffffffffff0fff3cff32ffff794d4815ffff02ffff6fffff40ffff7d');
+-- create user with name "testuser2" and password "password2"
+INSERT INTO users (name, password) VALUES ('testuser2', 'ffffff3cffff151050ffff1cff1807ffff777cffffffff11ff67ff65ffff315c');
+
+INSERT INTO acl_users (username, ng) VALUES ('testuser', 'alt.wendzelnntpd.test');
+INSERT INTO acl_users (username, ng) VALUES ('testuser2', 'alt.wendzelnntpd.test.empty');
+INSERT INTO acl_roles (role, ng) VALUES ('testrole', 'alt.wendzelnntpd.test.post');
+INSERT INTO users2roles (username, role) VALUES ('testuser', 'testrole');
